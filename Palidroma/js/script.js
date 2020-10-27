131fab9615e48859f354abecc76ea9150208d277
Othman Taik <taikothman1997@gmail.com>
2020-10-27 17:36:04 +0100

    Both Scripts need to be fixed

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,1 +1,17 @@
+var userWord = prompt("Inserisci parola");
 
+function stringaInversa (userWord) {
+  var wordString = userWord.length;
+  var start = userWord.substring(0, Math.floor(wordString/2)).toLowerCase();
+  var end = userWord.substring(wordString - Math.floor(wordString/2)).toLowerCase();
+  var flip = end.split("").reverse().join("");
+  var message = "Non è una PALIDROMA";
+
+  if (start === flip) {
+    message = "La parola è una PALIDROMA"
+  } else {
+    message
+  }
+}
+
+document.getElementById('result').innerHTML = "ciao" + message;
--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,1 +1,29 @@
+var parioDispari = prompt("Scrivi pari o dispari");
+var sceltaNumerogiocatore = parseInt(prompt("scrivi un numero da 1 a 5"));
+var sceltaNumeroComputer = getRandomIntInclusive(1,5);
+var numero = console.log(sceltaNumeroComputer);
+var sommaNumeri = sceltaNumerogiocatore + sceltaNumeroComputer;
+var message = "E' pari"
+document.getElementById('userNumber').innerHTML = sceltaNumerogiocatore
+document.getElementById('pcNumber').innerHTML = sceltaNumeroComputer
 
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+if (sommaNumeri % 2 == 0) {
+  message
+} else {
+  message = "E' dispari"
+}
+
+document.getElementById('result').innerHTML = message
+
+if (parioDispari === message){
+  document.getElementById('finalResult').innerHTML = "YOU WIN!"
+} else {
+  document.getElementById('finalResult').innerHTML = "YOU LOSE!"
+}
